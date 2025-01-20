d9e783c908b1b1d1bfd3989661a3d08d4ec087fd
shreyask5 <shreyasksh5@gmail.com>
2025-01-20 13:02:45 +0530

    Pinterest Clone API Upload

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.post('/upload-to-s3', upload.single('file'), async (req, res) => {
 
 // Route all other requests to the React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
 // Start the server
